f3d2bfdcb0d405d2b0a915d0a80301891547f506
DavidO28 <damers22@yahoo.com>
2024-11-12 13:40:43 +0400

    added pagination

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,53 @@
 <template>
   <div v-if="dataReady" class="container">
-    {{ sourceData.threads }}
+    <div v-for="thread in paginatedThreads" :key="thread.id">
+      {{ thread.title }}
+    </div>
+
+    <div class="pagination">
+      <button @click="goToPage(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="goToPage(currentPage + 1)" :disabled="currentPage === totalPages">Next</button>
+    </div>
   </div>
   <span v-else>Loading page data...</span>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import sourceData from '@/data.json'
 import { useDataReady } from '@/mixins/asyncDataStatus'
 
 const { dataReady, dataIsReady } = useDataReady()
 
+const itemsPerPage = 10
+
+const currentPage = ref(1)
+
+const paginatedThreads = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage
+  const endIndex = currentPage.value * itemsPerPage
+  return sourceData.threads.slice(startIndex, endIndex)
+})
+
+const totalPages = computed(() => {
+  return Math.ceil(sourceData.threads.length / itemsPerPage)
+})
+
 const fetchData = async () => {
   try {
     // simulating async request
-    await new Promise(resolve => setTimeout(resolve, 1000))  
+    await new Promise(resolve => setTimeout(resolve, 1000))
   } catch (e) {
     console.log(e)
   } finally {
     dataIsReady()
+  }
+}
+
+const goToPage = (page) => {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page
   }
 }
 
@@ -29,4 +57,20 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+button {
+  padding: 5px 10px;
+  margin: 0 5px;
+  cursor: pointer;
+}
+
+button:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
 </style>
